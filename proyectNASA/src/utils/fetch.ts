25320f49df9296d15dafc2c6d18240ceab1882a9
SantiRosso <santiagorosso@live.com.ar>
2023-10-24 21:48:45 -0300

    Home.tsx useEffect and useState

--- proyectNASA/src/utils/fetch.ts
+++ proyectNASA/src/utils/fetch.ts
@@ -1,7 +1,7 @@
 const API_KEY = 'HoLrTeP4iGd6UL8LmLqLUFA0vw5Zesz4ELEcyoiY';
 const API_URL = 'https://api.nasa.gov/planetary/apod';
 
-export default async (urlParams: string) => {
+export default async (urlParams?: string) => {
   try {
     const response = await fetch(
       `${API_URL}?api_key=${API_KEY}${
--- proyectNASA/src/views/Home/Home.tsx
+++ proyectNASA/src/views/Home/Home.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import Header from '../../components/Header';
+import fetchApi from '../../utils/fetch';
 
 const Home = () => {
+
+    const [todaysImage, setTodaysImage] = useState();
+
+    useEffect(() => {
+        const loadTodaysImage = async () => {
+            try {
+                const todaysImageResponse = await fetchApi();
+                setTodaysImage(todaysImageResponse);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        loadTodaysImage().catch(null)
+    }, [])
+
+    console.log(todaysImage)
+
     return(
         <View style={styles.container}>
             <Header/>
